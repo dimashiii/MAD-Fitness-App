d5b189d8161fc4a4d8868d06b9974dfd3b3fdfb6
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-08-13 21:07:35 +0530

    # comment comment added in goal java

---
+++ app/src/main/java/com/example/fitnessapp/ActivityDailygoal1.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//activity in goal
+public class ActivityDailygoal1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dailygoal1);
+    }
+}
