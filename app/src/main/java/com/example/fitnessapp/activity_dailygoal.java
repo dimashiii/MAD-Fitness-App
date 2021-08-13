251a377c3908421ec6eef8698f70fd96e2fb24d2
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-08-13 18:48:32 +0530

    initial commit

---
+++ app/src/main/java/com/example/fitnessapp/activity_dailygoal.java
@@ -0,0 +1,12 @@
+package com.example.fitnessapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_dailygoal extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dailygoal);
+    }
+}
