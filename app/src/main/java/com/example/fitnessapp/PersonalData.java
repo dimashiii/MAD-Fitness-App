c02a478ace4655fe7fd48b4f4d793efed7f327df
dimashiii <kdimashi1@gmail.com>
2021-09-18 18:49:11 +0530

    commit - initial commit and push

---
+++ app/src/main/java/com/example/fitnessapp/PersonalData.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data);
+    }
+}
