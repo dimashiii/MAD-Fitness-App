18763d667fbc1c6beac0839622851e414d8627ee
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-09-22 21:05:17 +0530

    files adding

--- app/src/main/java/com/example/fitnessapp/GoalsoverviewActivity.java
+++ app/src/main/java/com/example/fitnessapp/GoalsoverviewActivity.java
@@ -1,44 +1,14 @@
 package com.example.fitnessapp;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.text.TextUtils;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.fitnessapp.databinding.ActivityGoalsoverviewBinding;
-
-import java.util.UUID;
+import android.os.Bundle;
 
 public class GoalsoverviewActivity extends AppCompatActivity {
-    String uniqueID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityGoalsoverviewBinding binding = ActivityGoalsoverviewBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        SharedPreferences pref = getSharedPreferences("fitnessPref", Activity.MODE_PRIVATE);
-        uniqueID = pref.getString("uuid", "");
-
-        if (TextUtils.isEmpty(uniqueID)) {
-            uniqueID = UUID.randomUUID().toString();
-            pref.edit().putString("uuid", uniqueID).apply();
-        }
-
-        binding.start.setOnClickListener(v -> {
-
-
-            startActivity(new Intent(this, DailygoalsActivity.class));
-
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
+        setContentView(R.layout.activity_goalsoverview);
     }
 }
