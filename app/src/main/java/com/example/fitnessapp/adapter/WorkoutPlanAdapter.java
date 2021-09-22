f14ddf716e24d6a3c694c157fd773d55511f2440
sajinikulara <kulari102@gmail.com>
2021-09-22 23:58:22 +0530

    #added pick category page

--- app/src/main/java/com/example/fitnessapp/adapter/WorkoutPlanAdapter.java
+++ app/src/main/java/com/example/fitnessapp/adapter/WorkoutPlanAdapter.java
@@ -1,1 +1,22 @@
+package com.example.fitnessapp.adapter;
 
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.example.fitnessapp.model.WorkoutPlanModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+/*public class WorkoutPlanAdapter extends FirebaseRecyclerAdapter<WorkoutPlanModel> {
+
+    FirebaseRecyclerOptions<WorkoutPlanModel> options;
+    Context context;
+
+    public WorkoutPlanAdapter(Context context,FirebaseRecyclerOptions Workout)
+
+
+
+
+}*/
