aa5cacfb5ed2a237e088bfc719bd6977a4b7b3ad
sajinikulara <kulari102@gmail.com>
2021-09-26 05:26:42 +0530

    #modified card selector

--- app/src/main/java/com/example/fitnessapp/CardSelector.java
+++ app/src/main/java/com/example/fitnessapp/CardSelector.java
@@ -1,14 +1,57 @@
 package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CardSelector extends AppCompatActivity {
+
+    CardView workouts;
+    CardView dailygoals;
+    CardView mealplan;
+    CardView health;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_selector);
+
+        dailygoals = (CardView)findViewById(R.id.dailygoals);
+        mealplan = (CardView)findViewById(R.id.mealPlan);
+        health = (CardView)findViewById(R.id.health);
+        workouts = (CardView)findViewById(R.id.workout);
+
+        //dailygoal
+        dailygoals.setOnClickListener(view->{
+            Intent intentgoals = new Intent(this, GoalsoverviewActivity.class);
+
+            startActivity(intentgoals);
+        });
+
+        //mealPlan--> thissa
+       /* mealplan.setOnClickListener(view->{
+            Intent intentmeals = new Intent(this, MealJournal.class);
+
+            startActivity(intentmeals);
+        });*/
+
+        //health
+
+        health.setOnClickListener(view->{
+            Intent intenthealth = new Intent(this, HealthJournal.class);
+
+            startActivity(intenthealth);
+        });
+
+        //workout
+        workouts.setOnClickListener(view->{
+            Intent intentworkouts = new Intent(this, CreateWorkoutPlan.class);
+
+            startActivity(intentworkouts);
+        });
     }
+
+
 }
