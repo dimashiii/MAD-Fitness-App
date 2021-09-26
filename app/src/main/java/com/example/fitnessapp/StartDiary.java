5f13f7a5f10a442b15f7643598979a3c655480ce
dimashiii <kdimashi1@gmail.com>
2021-09-26 13:00:51 +0530

    #added bmi diary model

---
+++ app/src/main/java/com/example/fitnessapp/StartDiary.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartDiary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_diary);
+    }
+}
