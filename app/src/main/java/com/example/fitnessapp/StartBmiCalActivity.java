dc1abd01783b20d6993b8522d8a30b9e2a686f5e
dimashiii <kdimashi1@gmail.com>
2021-09-26 05:54:23 +0530

    #added mid page to cal the bmi

---
+++ app/src/main/java/com/example/fitnessapp/StartBmiCalActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartBmiCalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_bmi_cal);
+    }
+}
